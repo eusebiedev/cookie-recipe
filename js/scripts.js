d39446cfd940f7ee5b1548805f85f7e12808634d
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-21 17:01:49 -0700

    Add Javascript and CSS  files for event handler practice

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// User Interface Logic
+window.onload = function() {
+  let body = document.querySelector("body");
+  button1.onclick = function() {
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+  };
+  button2.onclick = function() {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+  };
+
+};
