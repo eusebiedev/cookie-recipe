993876b766b26da88ba5e456cb44ef8f3a9d6726
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-21 21:31:01 -0700

    Add 2 functions to scripts.js file to show/hide image

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 // User Interface Logic
 window.onload = function() {
   let body = document.querySelector("body");
-  button1.onclick = function() {
-  body.style.backgroundColor = "black";
-  body.style.color = "white";
+    button1.onclick = function() {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
   };
-  button2.onclick = function() {
-  body.style.backgroundColor = "white";
-  body.style.color = "black";
+    button2.onclick = function() {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
   };
   let image = document.querySelector("image");
-  button3.onclick = function () { 
-  document.getElementById("image").style.display = "none";
+    button3.onclick = function () { 
+    document.getElementById("image").style.display = "none";
   }
-  button4.onclick = function () { 
-  document.getElementById("image").style.display = "";
-    }
+    button4.onclick = function () { 
+    document.getElementById("image").style.display = "";
+  }
   
 };
